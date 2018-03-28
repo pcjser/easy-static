6701f363db0299d97969f9af133383ac95b5ebac
SWAIN <201688080@qq.com>
2018-03-28 13:42:40 +0800

    源样式添加css支持

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,22 @@ var del = require('del');
 
 gulp.task('default', ['serve']);
 
+gulp.task('css', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.init())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({
+      stream: true
+    }));
+})
+
 gulp.task('sass', function () {
   return gulp.src(['src/sass/*.scss', 'src/sass/*.sass'])
     .pipe(sass())
@@ -32,7 +48,7 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('dist/css'))
     .pipe(reload({
       stream: true
-    }))
+    }));
 })
 
 gulp.task('less', function () {
@@ -49,7 +65,7 @@ gulp.task('less', function () {
     .pipe(gulp.dest('dist/css'))
     .pipe(reload({
       stream: true
-    }))
+    }));
 })
 
 gulp.task('js', function () {
@@ -95,14 +111,14 @@ gulp.task('image', function () {
     ], {
       verbose: true
     }))
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('dist/images'));
 })
 
 gulp.task('delDist', function () {
-  del('./dist')
+  del('./dist');
 })
 
-gulp.task('serve', ['delDist', 'sass', 'js', 'image', 'less'], function () {
+gulp.task('serve', ['delDist', 'css', 'sass', 'js', 'image', 'less'], function () {
   browserSync.init({
     files: ['**'],
     server: {
@@ -110,7 +126,8 @@ gulp.task('serve', ['delDist', 'sass', 'js', 'image', 'less'], function () {
       index: './index.html'
     },
     port: 8888
-  })
+  });
+  gulp.watch('src/css/*.css', ['css']);
   gulp.watch(['src/sass/*.scss', 'src/sass/*.sass'], ['sass']);
   gulp.watch('src/less/*.less', ['less']);
   gulp.watch('src/js/*.js', ['js']);
